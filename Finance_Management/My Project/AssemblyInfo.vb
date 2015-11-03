50b220a7a858457f1a7dc251c88694dced3fe1d7
ifishman <iris-me@live.com>
2015-11-03 13:10:01 +0800

    inital

---
+++ Finance_Management/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的一般信息由以下
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+'查看程序集特性的值
+
+<Assembly: AssemblyTitle("企业财务管理")>
+<Assembly: AssemblyDescription("个人用的企业财务管理工具")>
+<Assembly: AssemblyCompany("im帥")>
+<Assembly: AssemblyProduct("企业财务管理")>
+<Assembly: AssemblyCopyright("Copyright © im帥 2015")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("0daa97bd-ed7c-4ab0-b03b-4d33c2a498b0")>
+
+' 程序集的版本信息由下列四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+'可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+' 方法是按如下所示使用“*”: :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("zh-CN")>
